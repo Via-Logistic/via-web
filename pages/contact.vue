c4819b57a3e32846c9f145bdba3959a5a1f2f581
Elif <2003013054@student.beykent.edu.tr>
2023-10-30 18:39:05 +0300

    contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,7 +7,7 @@
         <div class="section">
             <!--Google Map Area Start-->
             <div class="google-map-area w-100">
-                <iframe class="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2136.986005919501!2d-73.9685579655238!3d40.75862446708152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258e4a1c884e5%3A0x24fe1071086b36d5!2sThe%20Atrium!5e0!3m2!1sen!2sbd!4v1585132512970!5m2!1sen!2sbd"></iframe>
+                <iframe class="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2956.7306307968815!2d24.700708776029103!3d42.17744314661939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acce0623295acb%3A0x8a4bfb89514a491c!2sVIA%20LOGISTICS%20LTD!5e0!3m2!1str!2str!4v1698680017652!5m2!1str!2str"></iframe>
             </div>
             <!--Google Map Area Start-->
         </div>
@@ -21,16 +21,16 @@
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-12">
                             <div class="inner-content">
-                                <h1 class="title">Brooklyn, <span>New York</span></h1>
-                                <span class="text">Your address goes here <br> Your address goes here</span>
-                                <span class="email">Email: <a href="mailto:demo@example.com">demo@example.com</a></span>
+                                <h1 class="title">Rostov-on-Don <span>Rostov Region</span></h1>
+                                <span class="text">St. Privokzalnaya 4/1,  <br> </span>
+                                <span class="email">Email: <a href="mailto:info@vialogistic.ru">info@vialogistic.ru</a></span>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-12">
                             <div class="inner-info">
                                 <span class="call">Call directly:<br><a href="tel:0123456789">0123456789</a></span>
                                 <ul class="location-list">
-                                    <li><span>Brand Offices:</span>Office Location | Office Location | Office Location | Office Location <br> Office Location | Office Location</li>
+                                    <!-- <li><span>Brand Offices:</span>Office Location | Office Location | Office Location | Office Location <br> Office Location | Office Location</li> -->
                                     <li><span>Work Hours:</span>Mon - Sat: 8.00 - 17.00, Sunday closed</li>
                                 </ul>
                             </div>
