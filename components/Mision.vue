7ce80837520207bb7a77d21fb202cf32b8a417a1
Elif <2003013054@student.beykent.edu.tr>
2023-11-03 19:29:54 +0300

    vizyonumuz , visyonumuz bölümü - iletişim - hakkımızda yazısı güncellendi

---
+++ components/Mision.vue
@@ -0,0 +1,38 @@
+<template>
+          <div class="work-image-bg-about">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <div class="content">
+                    <h4 class="subtitle mb-5" data-aos="fade-up" data-aos-delay="300">
+                      Vizyonumuz ve Misyonumuz
+                    </h4>
+                    <h3 style="color: white" data-aos="fade-up" data-aos-delay="300">
+                              Türkiye, Azerbaycan, İran, OEA ve Çin gibi ülkelerde sürdürülebilir üretim yaparak,
+müşterilerimiz ve tedarikçilerimiz için değer yaratmaktayız. 
+                      <br />
+                      Ürün kalitemiz, geniş ürün gamımız
+ve etkin lojistik altyapımız ile sektöre öncülük etmeyi sürdürüyoruz.
+                              </h3>
+                     <p data-aos="fade-up" data-aos-delay="300">
+                     
+                    </p> 
+                     <!-- <a href="#" class="pdf-file" data-aos="fade-up" data-aos-delay="300"><span class="icon icofont-file-pdf"></span>Download Offer [.PDF]</a> --> 
+        
+                    <!-- <a
+                      href="https://www.youtube.com/watch?v=eS9Qm4AOOBY"
+                      target="_blank"
+                      class="video-box clearfix"
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                    >
+                      See How we work
+                      <span class="fa-play icofont-play"><i class="ripple"></i></span>
+                    </a>  -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+        
