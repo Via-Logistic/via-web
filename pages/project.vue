994f7cda5cfafbd1fec402b8e38f2b63f1d65819
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2023-11-04 12:41:35 +0300

    updated header and project

--- pages/project.vue
+++ pages/project.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="main-container">
-        <TheHeaderTwo />
-        <OffCanvasMobileMenu />
-        <Breadcrumb activeText="All Project" pageTitle="PRODUCT AND SERVICES" />
-        <GalleryWrapper />
-        <Footer />
-    </div>
+  <div class="main-container">
+    <TheHeaderTwo />
+    <OffCanvasMobileMenu />
+    <Breadcrumb activeText="All Project" pageTitle="PRODUCT AND SERVICES" />
+    <GalleryTwo />
+
+    <GalleryWrapper />
+    <Footer />
+  </div>
 </template>
 
-
+<script>
+import GalleryTwo from "@/components/GalleryTwo.vue";
+</script>
